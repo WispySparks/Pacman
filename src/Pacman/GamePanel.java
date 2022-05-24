31a9b2882693865b03c9ec0e7bbd49c03a697500
WispySparks <wispysparks@gmail.com>
2022-05-23 21:59:06 -0500

    pacman!

--- src/Pacman/GamePanel.java
+++ src/Pacman/GamePanel.java
@@ -14,64 +14,63 @@ public class GamePanel extends JPanel implements KeyListener, ActionListener{
     private final int tileSize = 16;
     private final int gridWidth = 28;
     private final int gridLength = 36;
-    private final File mapImage = new File("./resources/images/pacmap.png");
+    private final File mapFile = new File("./resources/images/pacmap.png");
     private boolean started = false;
-    private int direction = 0;
+    private int direction = 3;
     private Map map = new Map();
     private Pacman pacman = new Pacman(map);
     private JLabel mapLabel = new JLabel();
-    private BufferedImage bufferedImage;
+    private BufferedImage mapImage;
     private AudioPlayer audioPlayer = new AudioPlayer();
     private Timer timer = new Timer(50, this);
 
     GamePanel() {
         audioPlayer.playStart();
-        mapBG();
+        imageSetup();
         timer.start();
     }
 
     public void paint(Graphics g) {
         super.paint(g);
         g.setColor(Color.YELLOW);
-        g.fillOval(pacman.getX(), pacman.getY(), tileSize, tileSize);
-        g.setColor(Color.BLUE);
-        //g.fillRect(pacman.tempac.x, pacman.tempac.y, pacman.tempac.width, pacman.tempac.height);
-        g.setColor(Color.GREEN);
-        for (int i = 0; i<map.walls.length; i++) {
-            //g.fillRect(map.walls[i].x, map.walls[i].y, map.walls[i].width, map.walls[i].height);
-        }
+        g.drawImage(pacman.updateAnim(direction), pacman.getX(), pacman.getY(), null);
+        // g.setColor(Color.BLUE);
+        // g.fillRect(pacman.wallHitbox.x, pacman.wallHitbox.y, pacman.wallHitbox.width, pacman.wallHitbox.height);
+        // g.setColor(Color.GREEN);
+        // for (int i = 0; i<map.walls.length; i++) {
+        //     g.fillRect(map.walls[i].x, map.walls[i].y, map.walls[i].width, map.walls[i].height);
+        // }
     }
 
-    public void mapBG() {
+    public void imageSetup() {
         this.setBackground(Color.black);
         this.setPreferredSize(new Dimension(gridWidth*tileSize, gridLength*tileSize));
         try {
-            bufferedImage = ImageIO.read(mapImage);
+            mapImage = ImageIO.read(mapFile);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        ImageIcon mapIcon = new ImageIcon(bufferedImage);
+        ImageIcon mapIcon = new ImageIcon(mapImage);
         mapLabel.setIcon(mapIcon);
         this.add(mapLabel);
     }
 
     public void move() {
         if (pacman.checkCollision(direction) == true) {
-            
         }
         else {
             switch (direction) {
                 case 0: 
-                    pacman.up();
-                    break;
-                case 1:
                     pacman.right();
                     break;
-                case 2:
+                case 1:
                     pacman.down();
                     break;
-                case 3:
+                case 2:
                     pacman.left();
+                    break;
+                case 3:
+                    pacman.up();
                     break;
             }
         }
@@ -89,16 +88,16 @@ public class GamePanel extends JPanel implements KeyListener, ActionListener{
 
     public void keyPressed(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_UP) {
-            direction = 0;
+            direction = 3;
         }
         else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            direction = 1;
+            direction = 0;
         }
         else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-            direction = 2;
+            direction = 1;
         }
         else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-            direction = 3;
+            direction = 2;
         }
     }
     public void keyReleased(KeyEvent e) {
--- src/Pacman/Pacman.java
+++ src/Pacman/Pacman.java
@@ -1,19 +1,38 @@
 package Pacman;
 
 import java.awt.*;
+import java.io.File;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
 
 public class Pacman {
     
     private int xPos = 21 * 16;
     private int yPos = 20 * 16;
-    private int tempX = xPos;
-    private int tempY = yPos;
+    private int wallTempX = xPos;
+    private int wallTempY = yPos;
     private Rectangle[] walls;
-    Rectangle tempac = new Rectangle(tempX, tempY, 16, 16);
+    Rectangle wallHitbox = new Rectangle(wallTempX, wallTempY, 16, 16);
+    private final File[] animFiles = {new File("./resources/images/pacman_0.png"), new File("./resources/images/pacman_1.png"), new File("./resources/images/pacman_2.png"), new File("./resources/images/pacman_3.png"), new File("./resources/images/pacman_4.png")};
+    private final BufferedImage[] animImages = new BufferedImage[animFiles.length];
+    private int animState = 0;
     boolean isDead = false;
 
     Pacman(Map map) {
         this.walls = map.getWalls();
+        setupAnims();
+    }
+
+    public void setupAnims() {
+        for (int i = 0; i<animFiles.length; i++) {
+            try {
+                animImages[i] = ImageIO.read(animFiles[i]);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public int getX() {
@@ -26,27 +45,27 @@ public class Pacman {
 
     public boolean checkCollision(int direction) {
         switch (direction) {
-                case 0:
-                    tempY = yPos - 16;
-                    tempX = xPos;
-                    break;
-                case 1:
-                    tempX = xPos + 16;
-                    tempY = yPos;
-                    break;
-                case 2:
-                    tempY = yPos + 16;
-                    tempX = xPos;
-                    break;
-                case 3:
-                    tempX = xPos - 16;
-                    tempY = yPos;
-                    break;
-            } 
-        tempac.x = tempX;
-        tempac.y = tempY;
+            case 0:
+                wallTempX = xPos + 16;
+                wallTempY = yPos;
+                break;
+            case 1:
+                wallTempY = yPos + 16;
+                wallTempX = xPos;
+                break;
+            case 2:
+                wallTempX = xPos - 16;
+                wallTempY = yPos;
+                break;
+            case 3:
+                wallTempY = yPos - 16;
+                wallTempX = xPos;
+                break;
+        } 
+        wallHitbox.x = wallTempX;
+        wallHitbox.y = wallTempY;
         for (int i = 0; i<walls.length; i++) {
-            if (tempac.intersects(walls[i])) {
+            if (wallHitbox.intersects(walls[i])) {
                 return true;
             }
         }
@@ -69,4 +88,18 @@ public class Pacman {
         xPos -= 8;
     }
 
+    public BufferedImage updateAnim(int direction) {
+        BufferedImage anim = animImages[animState];
+        // rotates the image based on direction
+        BufferedImage rotatedImage = new BufferedImage(anim.getWidth(), anim.getHeight(), anim.getType()); 
+        AffineTransform transform = new AffineTransform();
+        transform.rotate(Math.toRadians(90*direction), anim.getWidth()/2, anim.getHeight()/2);
+        AffineTransformOp rotateOp = new AffineTransformOp(transform, AffineTransformOp.TYPE_BILINEAR);
+        BufferedImage rotatedAnim = rotateOp.filter(anim, rotatedImage);
+        animState++;
+        if (animState > 4 ){
+            animState = 0;
+        }
+        return rotatedAnim;
+    } 
 }
